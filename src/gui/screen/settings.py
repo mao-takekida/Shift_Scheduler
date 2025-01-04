117305038015dd91567ce18ccedffb044b935525
mao-takekida <mao_takekida@amp.i.kyoto-u.ac.jp>
2025-01-04 14:58:25 +0900

    add backbutton

--- src/gui/screen/settings.py
+++ src/gui/screen/settings.py
@@ -6,6 +6,21 @@ logger = logging.getLogger("shift_scheduler")
 
 
 class SettingsScreen:
+
+    # 戻るボタン
+    @staticmethod
+    def _back_button(page: ft.Page) -> ft.ElevatedButton:
+        # 循環参照を避けるため、ここでインポート
+        from gui.screen.main_screen import MainScreen
+
+        back_button = ft.ElevatedButton(
+            "戻る", on_click=lambda e: (page.clean(), MainScreen.show_main(page))
+        )
+        page.add(
+            back_button,
+        )
+        return back_button
+
     @staticmethod
     def show_settings(page: ft.Page) -> None:
         """
@@ -14,16 +29,12 @@ class SettingsScreen:
         Args:
             page (ft.Page): 設定画面を表示する Flet ページオブジェクト。
         """
-        # 循環参照を避けるため、ここでインポート
-        from gui.screen.main_screen import MainScreen
 
         page.scroll = ft.ScrollMode.AUTO
         page.vertical_alignment = ft.MainAxisAlignment.START
 
         # ボタン
-        back_button = ft.ElevatedButton(
-            "戻る", on_click=lambda e: (page.clean(), MainScreen.show_main(page))
-        )
+        back_button = SettingsScreen._back_button(page)
 
         page.add(
             back_button,
